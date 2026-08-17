b13481ceb0ecd98e71c997034f4153f401f07783
Jonatan1991 <75802929+Jonatan1991@users.noreply.github.com>
2026-08-17 20:21:10 +0200

    correccion en get_client_by_id refactorizar clients_by_country funciones en Sales_collection

--- src/client_collection.py
+++ src/client_collection.py
@@ -6,13 +6,15 @@ class Client_collection:
 
     def get_client_by_id(self, client_id: int):
         for client in self.clients:
-            if client.id == client_id:
+            if client.client_id == client_id:
                 return client
         return None
 
 
     def clients_by_country(self, country: str):
+        # clients = []
         for client in self.clients:
             if client.country == country:
+                # return clients.append(client)
                 return [client]
         return None
--- src/sales_collection.py
+++ src/sales_collection.py
@@ -1,8 +1,40 @@
 from sale import Sale
 
-class Sales_collection:
+class SalesCollection:
     def __init__(self, sales: list[Sale]):
         self.sales = sales
 
     def sales_by_client(self, client_id: int):
-        pass
+        sales = []
+        for sale in self.sales:
+            if sale.client_id == client_id:
+                sales.append(sale)
+
+        return sales 
+
+    def total_amount_by_client(self, client_id: int):
+        amount = 0
+        for sale in self.sales:
+            if sale.client_id == client_id:
+                amount += sale.amount
+        return amount
+
+    def total_amount_by_category(self, category: str):
+        amount = 0
+        for sale in self.sales:
+            if sale.category == category:
+                amount += sale.amount
+        return amount
+
+    def average_sale_by_client(self, client_id: int):
+        total = 0
+        contador = 0
+
+        for sale in self.sales:
+            if sale.client_id == client_id:
+                total += sale.amount
+                contador =+ 1
+
+        return total / contador
+
+    
