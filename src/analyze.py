4f82f3fe218ee7b41666c0b2ffd1d981544bdfb1
Jonatan1991 <75802929+Jonatan1991@users.noreply.github.com>
2026-08-20 18:58:58 +0200

    Agregar cálculos de ventas y clientes en la función generate_report; incluir clases y métodos para manejar ventas y ek segundo calculo

--- src/analyze.py
+++ src/analyze.py
@@ -1,7 +1,11 @@
 import json
 import pandas as pd
+
 from src.client import Client
 from src.client_collection import ClientCollection
+
+from src.sale import Sale
+from src.sales_collection import SalesCollection
 
 def generate_report():
 
@@ -11,8 +15,6 @@ def generate_report():
 
     with open(ruta_client, 'r', encoding='utf-8') as file:
         clients_data = json.load(file)
-
-   
 
 
     #convierto el json en un objeto
@@ -32,10 +34,29 @@ def generate_report():
     #Primer Calculo
     total_clientes = len(collection_client.clients)
 
+    sales_objects = []
+    sales_df = pd.read_csv(ruta_sales)
+
+    for col, row in sales_df.iterrows():
+        sale = Sale(
+            sale_id=row['sale_id'],
+            client_id=int(row['client_id']),
+            product=row['product'],
+            category=row['category'],
+            amount=float(row['amount']),
+            date=row['date']
+        )
+        sales_objects.append(sale)
+
+    sales_collection = SalesCollection(sales_objects)
+
+    # Segundo cálculo
+    total_ventas = len(sales_collection.sales)
+
     report = {
         "summary": {
             "total_clients": total_clientes,
-            "total_sales": 0,        # De momento en 0, lo calcularás en el siguiente paso
+            "total_sales": total_ventas,        # De momento en 0, lo calcularás en el siguiente paso
             "total_revenue": 0.0
         }
     }
